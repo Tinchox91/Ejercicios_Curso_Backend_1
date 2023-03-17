b507aaf6c3924df76df5c8026afb7892caabc6cb
Tincho <tinchox1991@gmail.com>
2023-03-17 06:42:34 -0300

    segundo commit

--- src/Primer_soft/Primer_clase.java
+++ src/Primer_soft/Primer_clase.java
@@ -4,9 +4,9 @@ import javax.swing.JOptionPane;
 public class Primer_clase {
     
   public static void main(String[] args) {
-      ejercicio_2 ejer1 = new ejercicio_2();
+      ejercicio_1 ejer1 = new ejercicio_1();
       
-      ejer1.ingresarNombre();
+      ejer1.ingresarNumero();
    
   }   
        
