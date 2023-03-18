a9e21218fdedb909921d9ef1bed980d84ec20520
Tincho <tinchox1991@gmail.com>
2023-03-18 01:04:16 -0300

    commit final

--- src/Guia_3/Estructuras_de_control.java
+++ src/Guia_3/Estructuras_de_control.java
@@ -152,6 +152,69 @@ public class Estructuras_de_control {
      }while (s1==false);     
       
 }
+        public void rs232(){
+            Scanner leer = new Scanner(System.in);
+            String cadena;
+            int val1,val2;
+            val1=0;
+            val2=0;
+            
+            String val = "&&";
+           do{
+           System.out.println("ingrese cadena");
+                do{
+                                cadena = leer.next();
+                                if (cadena.length()> 4){
+                                    System.out.println("La longitud de la cadena es incorrecto vuelva a ingresar");
+                                }
+            } while (cadena.length() > 4  );
+               
+                if (cadena.substring(0,1).equals("x") && cadena .substring(3,4).equals("o")){
+               val1= val1+1;
+                }
+                else{val2=val2+1;
+              
+           }
+                if (cadena.equals(val)){
+                    val2=val2-1;
+                    if (val2< 0) {
+                        val2=0;
+                    }
+                }
+                   
+                
+           }while  (!cadena.equals(val));
+            System.out.println("INFORME");
+                System.out.println("La cantidad de falsas son:"+ val2);
+                System.out.println("La cantidad de verdaderas son : "+ val1);
+            
+            
+            
+            
+            
+    
+} 
+        public void cuadrado(){
+           Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese la longitud del cuadrado: ");
+        int longitud = scanner.nextInt();
+
+        for (int i = 1; i <= longitud; i++) {
+            for (int j = 1; j <= longitud; j++) {
+                if (i == 1 || i == longitud || j == 1 || j == longitud) {
+                    System.out.print("*");
+                //} else if (j == (longitud) && i == (longitud)) {
+                   // System.out.print(" "); 
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+            
+            
+        
 
     public static void main(String[] args) {
         Estructuras_de_control ej1 = new Estructuras_de_control();
@@ -160,6 +223,8 @@ public class Estructuras_de_control {
          Estructuras_de_control ej4 = new Estructuras_de_control();
          Estructuras_de_control ej5 = new Estructuras_de_control();
          Estructuras_de_control ej6 = new Estructuras_de_control();
+         Estructuras_de_control ej7 = new Estructuras_de_control();
+         Estructuras_de_control ej8 = new Estructuras_de_control();
         int op;
         Scanner leer = new Scanner (System.in);     
         
@@ -189,7 +254,10 @@ public class Estructuras_de_control {
             case 6:
                 ej6.menu();
                 break;
-
+            case 7:
+                ej7.rs232();
+            case 8:
+                ej8.cuadrado();
 
         	default:
            System.out.println("El valor ingresado en la variable opcion es diferente" + "a todos los casos analizados por el switch");
