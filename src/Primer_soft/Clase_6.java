2792dfc3258db6eae2ff7d768fff1c7275aec1c6
Tincho <tinchox1991@gmail.com>
2023-03-28 21:57:47 -0300

    actualizacion de primer soft

--- src/Primer_soft/Clase_6.java
+++ src/Primer_soft/Clase_6.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Primer_soft;
 
-/**
- *
- * @author Tincho y Sofi
- */
 public class Clase_6 {
+    public void hola(){
+        System.out.println("hoolaa!!");
+    }
     
 }
--- src/Primer_soft/Primer_clase.java
+++ src/Primer_soft/Primer_clase.java
@@ -1,15 +1,111 @@
 
 package Primer_soft;
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 public class Primer_clase {
     
   public static void main(String[] args) {
       ejercicio_1 ejer1 = new ejercicio_1();
-      
-      ejer1.ingresarNumero();
+      ejercicio_2 ejer2 = new ejercicio_2();
+      Ejercicio_3 ejer3 = new Ejercicio_3();
+      Ejercicio_4 ejer4 = new Ejercicio_4();
+      Ejercicio_5 ejer5 = new Ejercicio_5();
+      Clase_6 cla6 = new Clase_6();
+      Scanner leer = new Scanner(System.in);
+        int op;
+        boolean val;
+        val = false;
+        String op2;
+        while (!val) {
+            System.out.println("elija opcion:");
+            System.out.println("1.Ingresar numero");
+            System.out.println("2.Ingresar nombre");
+            System.out.println("3.Convertir cadena");
+            System.out.println("4.Convertir f a grados");
+            System.out.println("5.Calculadora");
+            System.out.println("6.simetrica");
+            System.out.println("7.Salir");
+          
+            op = leer.nextInt();
+            switch (op) {
+                case 1:
+                     ejer1.ingresarNumero();
+                    System.out.println("desea continuar? S/N");
+                    op2 = leer.next();
+                    if (op2.equalsIgnoreCase("N") || op2.equalsIgnoreCase("n")) {
+                        val = true;
+                    } else {
+                        val = false;
+                    }
+
+                    break;
+                case 2:
+                    ejer2.ingresarNombre() ;
+                    System.out.println("desea continuar? S/N");
+                    op2 = leer.next();
+                    if (op2.equalsIgnoreCase("N") || op2.equalsIgnoreCase("n")) {
+                        val = true;
+
+                    } else {
+                        val = false;
+                    }
+                    break;
+                case 3:
+                    ejer3.convertircadena();
+                    String vale;
+                    boolean val1 = false;
+                    while (!val1) {
+                       
+                        System.out.println("desea continuar? S/N");
+                        vale = leer.next();
+                        if (vale.equalsIgnoreCase("n") || vale.equalsIgnoreCase("N")) {
+                            val1 = true;
+                        }
+                    }
+                    break;
+                    
+                case 4:
+                    ejer4.equit();
+                    System.out.println("desea continuar? S/N");
+                    op2 = leer.next();
+                    if (op2.equalsIgnoreCase("N") || op2.equalsIgnoreCase("n")) {
+                        val = true;
+                    } else {
+                        val = false;
+                    }
+                    break;
+                    
+                case 5:
+                    ejer5.calculadora();
+                    System.out.println("desea continuar? S/N");
+                    op2 = leer.next();
+                    if (op2.equalsIgnoreCase("N") || op2.equalsIgnoreCase("n")) {
+                        val = true;
+                    } else {
+                        val = false;
+                    }
+
+                    break;
+                case 6:cla6.hola();
+                System.out.println("desea continuar? S/N");
+                    op2 = leer.next();
+                    if (op2.equalsIgnoreCase("N") || op2.equalsIgnoreCase("n")) {
+                        val = true;
+                    } else {
+                        val = false;
+                         break;
+                    }
+
+               
+                default:
+                System.out.println("numero invalido");
+                
+            
+     
    
   }   
        
     
-    
+        }          
+  }
 }
